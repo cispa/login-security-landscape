7504b8614ea3212f51a7fd90b6049e3b9ff00e67
JannisBush <33023300+JannisBush@users.noreply.github.com>
2023-12-21 20:34:54 +0100

    Remove old postgres

--- PythonCrawler/src/entrypoint.sh
+++ PythonCrawler/src/entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# make sure playwright is in path
+# Make sure playwright is in path
 export PATH=$PATH:/home/pycrawler/.local/bin
 
-# setup database
-sudo service postgresql restart
-sudo -u postgres psql -tc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRES_DB';" | grep -q 1 || sudo -u postgres psql -c "CREATE DATABASE $POSTGRES_DB;"
-sudo -u postgres psql $POSTGRES_DB -c "ALTER USER postgres WITH PASSWORD '$(cat $POSTGRES_PASSWORD_FILE | tr -d '\n')';"
-# dummy run which will initialize the database
+# Initialize the database
 PYTHONPATH=/pycrawler/demo/headers:/pycrawler python3 main.py -m "InclusionIssues HeadersExperiment" -j null -c 1
 
 # Start remote desktop (Xvfb, fluxbox and x11vnc)
